c900957338bccd42081a3775ff3b08f223e0b764
fatihgunalp <fatih.gunalp@gmail.com>
2025-12-13 11:16:11 +0300

    oop encapsulation

---
+++ 13.12/OOP_Introduction/OOP_Introduction/Bilgisayar.cs
@@ -0,0 +1,65 @@
+﻿
+
+namespace OOP_Introduction
+{
+    internal class Bilgisayar
+    {
+        //Bir bilgisayarın ...'sı olur.
+        //EkranKartı
+        //Anakartı
+        //Islemci
+
+
+        //field: alan
+        //field varsayılan erişim belirteci private
+
+        //public: umumi, açık her yerden ulaşılır.
+        //internal: sadece ait olduğu olduğu projeden ulaşılır.
+        //protected: sadece miras alınan class içinden ulaşılır.
+        //protected internal: sadece ait olduğu projede ve miras alınan class'dan ulaşılır.
+
+
+
+        //oluşturulan bütün bilgisayarların anakart markası "asus" olsun. Eğer bu değer harici bir değer gönderilirse kullanıcıya "yanlış marka" mesajı iletilsin.
+
+        #region Encapsulation
+        string _anakartMarka;
+
+        public string AnaKartMarka
+        {
+
+            get { return _anakartMarka; }
+
+            set
+            {
+                if (value == "asus")
+                {
+                    _anakartMarka = value;
+                }
+                else
+                {
+                    _anakartMarka = "tanımsız marka";
+                }
+            }
+        } 
+        #endregion
+
+
+
+
+        public string EkranKartMarka { get; set; }
+        public string IslemciMarka { get; set; }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ 13.12/OOP_Introduction/OOP_Introduction/Program.cs
@@ -0,0 +1,34 @@
+﻿
+//OOP (Nesne Yönelimli Programlama)
+
+//Encapsulation: Kapsulleme
+//Inheritance: Kalıtım/Miras
+//Polymorphism: Çok biçimlilik
+//Abstraction: Soyutlama
+
+
+//Class: içerisinde birden fazla değeri barındrıan referans tip olarak yaşamını sürdüren nesnelerdir.
+
+
+using OOP_Introduction;
+
+//Bilgisayar pc1 = new Bilgisayar();
+//pc1.anaKartMarka = "msi";
+
+//pc1.ekranKartMarka = "Nvidia RTX";
+//pc1.islemciMarka = "AMD";
+
+//Console.WriteLine($"anakart marka: {pc1.anaKartMarka}\n Ekran kart: {pc1.ekranKartMarka}\n İşlemci: {pc1.islemciMarka}");
+
+
+//Bilgisayar pc2 = new Bilgisayar();
+//pc2.anaKartMarka = "asus";
+
+//Console.WriteLine($"anakart marka: {pc2.anaKartMarka}\n");
+
+Bilgisayar pc1=new Bilgisayar();
+pc1.EkranKartMarka = "Nvidia";
+pc1.AnaKartMarka = "asus";
+
+
+
