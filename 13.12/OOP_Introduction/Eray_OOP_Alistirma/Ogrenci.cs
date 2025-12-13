8379c932261cb379a0957b0d5063fd16d53201c0
Eray Emeklier <erayemeklier@gmail.com>
2025-12-14 00:26:15 +0300

    Alistirmalar eklendi

---
+++ 13.12/OOP_Introduction/Eray_OOP_Alistirma/Ogrenci.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eray_OOP_Alistirma
+{
+    internal class Ogrenci:Kisi
+    {
+        public string Numara { get; set; }
+        public List<string> Dersler { get; set; }
+        public Ogrenci(string ad, int yas, string numara)
+        {
+            Ad = ad;
+            Yas = yas;
+            Numara = numara;
+            Dersler = new List<string>();
+        }
+
+        public override void Tanit()
+        {
+            Console.WriteLine("Benim adım " + Ad + ", " + Yas + " yaşındayım. Numaram: " + Numara);
+        }
+        public void DersleriGoster()
+        {
+            Console.WriteLine(Ad + " adlı öğrencinin dersleri:");
+            foreach (var ders in Dersler)
+            {
+                Console.WriteLine("- " + ders);
+            }
+        }
+
+
+
+
+    }
+}
---
+++ 13.12/OOP_Introduction/Eray_OOP_Alistirma/Program.cs
@@ -0,0 +1,13 @@
+﻿using Eray_OOP_Alistirma;
+
+Ogrenci ogrenci1 = new Ogrenci("Murat", 15, "O123");
+Ogretmen ogretmen1 = new Ogretmen("Mehmet", 49, "Matematik");
+
+ogrenci1.Tanit();
+ogretmen1.Tanit();
+
+ogrenci1.Dersler.Add("Matematik");
+ogrenci1.Dersler.Add("Fizik");
+ogrenci1.DersleriGoster();
+
+Console.ReadLine();
