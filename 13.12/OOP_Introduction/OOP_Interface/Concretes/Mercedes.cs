994bd146b146dade705290a8572d5127c8557ebb
fatihgunalp <fatih.gunalp@gmail.com>
2025-12-14 10:36:56 +0300

    interface

---
+++ 13.12/OOP_Introduction/OOP_Interface/Concretes/Mercedes.cs
@@ -0,0 +1,22 @@
+﻿using OOP_Interface.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Interface.Concretes
+{
+    internal class Mercedes : Otomobil, IYetenek,IYakit
+    {
+        public override string Maxhiz()
+        {
+            return "Max hız 300km/h";
+        }
+
+        public string Otopilot()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 13.12/OOP_Introduction/OOP_Interface/Program.cs
@@ -0,0 +1,13 @@
+﻿//Otomobil nesne oluşturalım.
+//Audi marka otomobil
+//Mercedes marka otomobil
+
+using OOP_Interface.Concretes;
+
+Audi audi=new Audi();
+
+Mercedes mercedes=new Mercedes();
+mercedes.Otopilot();
+
+Tesla tesla=new Tesla();
+tesla.Otopilot();
