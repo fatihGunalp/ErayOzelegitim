9694380d74814f3cd3362e4679f7992cd743fefa
fatihgunalp <fatih.gunalp@gmail.com>
2025-12-14 14:09:32 +0300

    ef core codefirst

---
+++ 14.12/DataAccess_CodeFirst/DataAccess_CodeFirst/Program.cs
@@ -0,0 +1,34 @@
+﻿
+//Nuget Package Manager
+//Microsoft.EntityFrameworkCore.SqlServer
+//Microsoft.EntityFrameworkCore.Tools
+/*
+ Add-Migration
+Bundle-Migration
+Drop-Database
+Get-DbContext
+Get-Migration
+Optimize-DbContext
+Remove-Migration
+Scaffold-DbContext
+Script-Migration
+Update-Database
+ 
+ */
+
+
+//Entities
+//1-Product
+//2-Category
+
+//Context
+//içeerisinde entityleri barındıran veritabanı nesnesidir.
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
