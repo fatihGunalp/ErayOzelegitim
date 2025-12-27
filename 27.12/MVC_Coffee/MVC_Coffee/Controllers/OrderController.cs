c65a66c1662f517b176a08cf64e50f9ec8b19fea
fatihgunalp <fatih.gunalp@gmail.com>
2025-12-27 13:21:58 +0300

    sipariş controllerı oluşturuldu.

---
+++ 27.12/MVC_Coffee/MVC_Coffee/Controllers/OrderController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVC_Coffee.Controllers
+{
+    public class OrderController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
